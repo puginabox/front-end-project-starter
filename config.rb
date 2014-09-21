c8b7a27f6fe8731fec17c81b56b3ed57e8fc9b5c
Griffin Byron <puginabox@gmail.com>
2014-09-21 10:24:40 -0400

    adding compass config

--- config.rb
+++ config.rb
@@ -2,5 +2,14 @@
 css_dir = 'css'
 sass_dir = 'dev/sass'
 javascripts_dir = 'js'
-output_style = :compressed
+# output_style = :compressed
+output_style = :expanded
 relative_assets = true;
+# environment = :development
+
+http_path = "/"
+css_dir = "css"
+sass_dir = "scss"
+images_dir = "images"
+javascripts_dir = "js"
+
